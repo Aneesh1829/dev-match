67bcceffb4e906b1e1b3cb790501627e3b4a141c
Aneesh1829 <aneeshreddy005@gmail.com>
2026-09-03 18:44:23 +0530

    Configure MySQL SSL

--- server/config/database.js
+++ server/config/database.js
@@ -10,18 +10,29 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
     pool: {
       max: 10,
       min: 0,
       acquire: 30000,
       idle: 10000,
     },
+
     define: {
       timestamps: true,
       underscored: true,
     },
   }
 );
+
+module.exports = sequelize;
 
 const connectDB = async () => {
   try {
